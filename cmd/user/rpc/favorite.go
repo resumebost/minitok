3ae9089e3c982b94511139710d94e3e1a2fae96c
Nefelibata <121291112+Nefelibata0402@users.noreply.github.com>
2023-09-06 21:11:40 +0800

    feat:初始化使用的rpc接口

---
+++ cmd/user/rpc/favorite.go
@@ -0,0 +1,69 @@
+package rpc
+
+import (
+	"context"
+	"minitok/internal/constant"
+	"minitok/internal/middleware"
+	"minitok/internal/unierr"
+	"minitok/kitex_gen/favorite"
+	"minitok/kitex_gen/favorite/favoriteservice"
+	"time"
+
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/cloudwego/kitex/pkg/retry"
+	etcd "github.com/kitex-contrib/registry-etcd"
+	opentracing "github.com/kitex-contrib/tracer-opentracing"
+)
+
+var favoriteClient favoriteservice.Client
+var favoriteConstants = &constant.AllConstants.FavoriteService
+
+func initFavoriteRPC() {
+	r, err := etcd.NewEtcdResolver([]string{constant.ETCDAddress})
+	if err != nil {
+		klog.Fatalf("ETCD resolver initialization failed: %v", err)
+	}
+
+	c, err := favoriteservice.NewClient(
+		favoriteConstants.Name,
+		client.WithMiddleware(middleware.CommonMiddleware),
+		client.WithInstanceMW(middleware.ClientMiddleware),
+		client.WithMuxConnection(1),
+		client.WithRPCTimeout(3*time.Second),
+		client.WithConnectTimeout(50*time.Millisecond),
+		client.WithFailureRetry(retry.NewFailurePolicy()),
+		client.WithSuite(opentracing.NewDefaultClientSuite()),
+		client.WithResolver(r),
+	)
+
+	if err != nil {
+		klog.Fatalf("Kitex client initialization failed: %v", err)
+	}
+
+	favoriteClient = c
+}
+
+func FavoriteCount(ctx context.Context, req *favorite.CountRequest) (*favorite.CountResponse, error) {
+	resp, err := favoriteClient.Count(ctx, req)
+	if err != nil {
+		return resp, err
+	}
+
+	if resp.StatusCode != 0 {
+		return nil, unierr.NewErrCore(resp.StatusCode, resp.StatusMsg)
+	}
+	return resp, nil
+}
+
+func FavoriteCountByUser(ctx context.Context, req *favorite.CountByUserRequest) (*favorite.CountByUserResponse, error) {
+	resp, err := favoriteClient.CountByUser(ctx, req)
+	if err != nil {
+		return resp, err
+	}
+
+	if resp.StatusCode != 0 {
+		return nil, unierr.NewErrCore(resp.StatusCode, resp.StatusMsg)
+	}
+	return resp, nil
+}
